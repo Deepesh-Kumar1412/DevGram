dc6b65c10bd6b0d8d9224d3130c15287018bae39
Deepesh Kumar1412 <kumardeepesh323@gmail.com>
2025-10-17 12:46:27 +0530

    route handlers

--- src/app.js
+++ src/app.js
@@ -2,28 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ firstname: "Deepesh", lastname: "Kumar" });
-});
-
-app.post("/user", (req, res) => {
-  res.send("Hello from the post server");
-});
-
-app.patch("/user", (req, res) => {
-  res.send("Hello from the patch server");
-});
-
-app.delete("/user", (req, res) => {
-  console.log("Deleted successfully");
-  res.send("Hello from the delete server");
-});
-
-
-app.use("/", (req, res) => {
-  res.send("Welcome to the server");
-});
-
+app.use(
+  "/",
+  (req, res, next) => {
+    res.send("Welcome to the server");
+    next();
+  },
+  (req, res, next) => {
+    res.send("The second response");
+  }
+);
 
 app.listen(7777, () => {
   console.log("Server is listening");
